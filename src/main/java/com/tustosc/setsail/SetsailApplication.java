cf766d3cf12fe02e3b117fdf126c0f0baccff2da
Mike Solar <iam@mikesolar.cn>
2025-09-06 18:04:34 +0800

    创建项目

---
+++ src/main/java/com/tustosc/setsail/SetsailApplication.java
@@ -0,0 +1,13 @@
+package com.tustosc.setsail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetsailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetsailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tustosc/setsail/SetsailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tustosc.setsail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetsailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
