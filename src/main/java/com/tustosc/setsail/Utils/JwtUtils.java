f5f82ec9c405f0e79cfbc460dd5d4fb66361abd6
Mike Solar <iam@mikesolar.cn>
2025-09-07 20:50:44 +0800

    add:JWTUtils

--- src/main/java/com/tustosc/setsail/Utils/JwtUtils.java
+++ src/main/java/com/tustosc/setsail/Utils/JwtUtils.java
@@ -21,18 +21,23 @@ public class JwtUtils {
         return header+"."+payload+"."+sign;
     }
 
+    public String getUuidFromJwt(String jwt){
+        return "";
+    }
+
     private String buildHeader() throws JSONException {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("alg", "HS256");
         jsonObject.put("typ", "JWT");
         String header = jsonObject.toString();
+        // 编码Base64，解码基本就是Encoder变Decoder
         Base64.Encoder encoder = Base64.getEncoder();
         return encoder.encodeToString(header.getBytes());
     }
 
     private String buildPayload(String uuid) throws JSONException {
         JSONObject jsonObject = new JSONObject();
-        jsonObject.put("sub", uuid);
+        jsonObject.put("sub", uuid); // 这是要解码出来的内容
         jsonObject.put("exp", System.currentTimeMillis()+24*60*60*1000);
         Random random=new Random();
         jsonObject.put("jit", String.valueOf(random.nextInt(10)));
