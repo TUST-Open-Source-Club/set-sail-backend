f4591077b2c70545ff1e8d05c42198fb74def1b6
Sun Yu <iam@mikesolar.cn>
2025-09-18 20:39:24 +0800

    Revert "add: 课程和用户信息"

--- src/main/java/com/tustosc/setsail/Mappers/LearningPathMapper.java
+++
@@ -1,9 +0,0 @@
-package com.tustosc.setsail.Mappers;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tustosc.setsail.Entiy.LearningPath;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface LearningPathMapper extends BaseMapper<LearningPath> {
-}
