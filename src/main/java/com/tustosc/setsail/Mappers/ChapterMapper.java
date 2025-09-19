129c287f98c540e40fe1b96625935be11fde3742
Mike Solar <iam@mikesolar.cn>
2025-09-19 18:15:09 +0800

    fix:添加测试，修复bug 1. 添加@Mapper注解，此前遗漏； 2. 修复JwtUtils的Bug； 3. 添加回HmacSHA256Utils； 4. 将application.yml加入gitignore，改为提交模板。

--- src/main/java/com/tustosc/setsail/Mappers/ChapterMapper.java
+++ src/main/java/com/tustosc/setsail/Mappers/ChapterMapper.java
@@ -2,6 +2,8 @@ package com.tustosc.setsail.Mappers;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.tustosc.setsail.Entiy.Chapter;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ChapterMapper extends BaseMapper<Chapter> {
 }
